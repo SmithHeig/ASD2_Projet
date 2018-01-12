fc26a54f541ff6257606ea4920f2db9a3dbef707
Unknown <james.smith@heig-vd.ch>
2018-01-12 09:21:30 +0100

    Start Project
    A l'attaqqqqqqqqqqqqquuuuuuuuuue

---
+++ ASD2_Projet/Corrector.h
@@ -0,0 +1,16 @@
+/* 
+ * File:   Corrector.h
+ * Author: Allemand Adrien, Chatillon Jeremie and James Smith
+ *
+ * Created on 12 janvier 2018, 09:18
+ */
+
+#ifndef CORRECTOR_H
+#define CORRECTOR_H
+
+class Corrector {
+    
+};
+
+#endif /* CORRECTOR_H */
+
